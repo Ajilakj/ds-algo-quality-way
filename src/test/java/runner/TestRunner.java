ed2b2faecb11b88bf0e39ef0704a80ce15e88c78
Ajila K Johnkutty <kjajila@gmail.com>
2023-09-15 17:14:13 -0400

    add TestRunner class in runner package

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,30 @@
+package runner;
+
+import org.junit.runner.RunWith;
+import org.testng.annotations.DataProvider;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+
+	@RunWith(Cucumber.class) //Junit execution
+
+		@CucumberOptions(
+				plugin = {"pretty", "html:target/quality_way.html"}, //reporting purpose
+				monochrome=false,  //console output color
+				tags = "@tag", //tags from feature file
+				features = {"src/test/resources/features"}, //location of feature files - folder name
+				glue= "stepDefinition") //location of step definition files - package name
+
+
+		public class TestRunner extends AbstractTestNGCucumberTests{
+			
+			@Override
+		    @DataProvider(parallel = false)
+		    public Object[][] scenarios() {
+						
+				return super.scenarios();
+		    }
+
+	}
