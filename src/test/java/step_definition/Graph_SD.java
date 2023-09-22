fdcc58669f980aae0e349f8ab9599e90c023027e
Ajila K Johnkutty <kjajila@gmail.com>
2023-09-21 22:57:09 -0400

    create test cases for graph/graph and try here text editor

--- src/test/java/step_definition/Graph_SD.java
+++ src/test/java/step_definition/Graph_SD.java
@@ -50,41 +50,41 @@ public class Graph_SD extends BaseClass {
 		UtlityFunctionsOBJ.check_url("https://dsportalapp.herokuapp.com/graph/");
 	}
 
-//	@Given("The user is in the Graph page")
-//	public void the_user_is_in_the_graph_page() {
-//		UtlityFunctionsOBJ.check_url("https://dsportalapp.herokuapp.com/graph/");
-//	}
-//
-//	@When("The user clicks Graph link")
-//	public void the_user_clicks_graph_link() {
-//		GraphOBJ.select_graph_from_left_link();
-//	}
-//
-//	@Then("The user should be directed to graph\\/graph Page")
-//	public void the_user_should_be_directed_to_graph_graph_page() {
-//		UtlityFunctionsOBJ.check_url("https://dsportalapp.herokuapp.com/graph/graph/");
-//	}
-//
-//	@When("The user clicks Try Here button")
-//	public void the_user_clicks_try_here_button() {
-//		GraphOBJ.click_try_here();
-//	}
-//
-//	@Then("The user should be redirected to a tryEditor page")
-//	public void the_user_should_be_redirected_to_a_try_editor_page() {
-//		UtlityFunctionsOBJ.check_url("https://dsportalapp.herokuapp.com/tryEditor");
-//	}
-//
-//	@When("some python as input then click run button")
-//	public void some_python_as_input_then_click_run_button() {
-//		GraphOBJ.try_editor_code();
-//
-//	}
-//
-//	@Then("The output will be display in the editor")
-//	public void the_output_will_be_display_in_the_editor() {
-//		GraphOBJ.check_output();
-//	}
+	@Given("The user is in the Graph page")
+	public void the_user_is_in_the_graph_page() {
+		UtlityFunctionsOBJ.check_url("https://dsportalapp.herokuapp.com/graph/");
+	}
+
+	@When("The user clicks Graph link")
+	public void the_user_clicks_graph_link() {
+		GraphOBJ.select_graph_from_left_link();
+	}
+
+	@Then("The user should be directed to graph\\/graph Page")
+	public void the_user_should_be_directed_to_graph_graph_page() {
+		UtlityFunctionsOBJ.check_url("https://dsportalapp.herokuapp.com/graph/graph/");
+	}
+
+	@When("The user clicks Try Here button")
+	public void the_user_clicks_try_here_button() {
+		GraphOBJ.click_try_here();
+	}
+
+	@Then("The user should be redirected to a tryEditor page")
+	public void the_user_should_be_redirected_to_a_try_editor_page() {
+		UtlityFunctionsOBJ.check_url("https://dsportalapp.herokuapp.com/tryEditor");
+	}
+
+	@When("some python as input then click run button")
+	public void some_python_as_input_then_click_run_button() {
+		GraphOBJ.try_editor_code();
+
+	}
+
+	@Then("The output will be display in the editor")
+	public void the_output_will_be_display_in_the_editor() {
+		GraphOBJ.check_output();
+	}
 //
 //	@When("The user clicks Graph Representations link")
 //	public void the_user_clicks_graph_representations_link() {
