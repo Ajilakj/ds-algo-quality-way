7ed1d54d7e2904ca51829f6ddb8a5d305a45e739
Bismitha <bismitha@gmail.com>
2023-09-26 02:39:37 -0700

    Array_POM

---
+++ src/test/java/step_definition/Arrays_SD.java
@@ -0,0 +1,160 @@
+package step_definition;
+
+import io.cucumber.java.en.*;
+
+public class Arrays_SD {
+
+	@Given("user is in home")
+	public void user_is_in_home() {
+
+
+	}
+
+	@When("clicks Get Started button under Array")
+	public void clicks_get_started_button_under_array() {
+	   
+
+	}
+
+	@Then("user is directed to array page")
+	public void user_is_directed_to_array_page() {
+	   
+
+	}
+
+	@Given("useris in array page")
+	public void useris_in_array_page() {
+	   
+
+	}
+
+	@Given("clicks Arrays in Python button")
+	public void clicks_arrays_in_python_button() {
+	   
+
+	}
+
+	@Then("user is directed to arrays-in-python page")
+	public void user_is_directed_to_arrays_in_python_page() {
+	   
+
+	}
+
+	@Then("user clicks Try here>>> button")
+	public void user_clicks_try_here_button() {
+	   
+
+	}
+
+	@Then("user is directed to tryEditor box")
+	public void user_is_directed_to_try_editor_box() {
+	   
+
+	}
+
+	@Given("user is already in tryeditor page")
+	public void user_is_already_in_tryeditor_page() {
+	   
+
+	}
+
+	@When("user enters valid code in tryEditor box")
+	public void user_enters_valid_code_in_try_editor_box() {
+	   
+
+	}
+
+	@When("clicks Run button")
+	public void clicks_run_button() {
+	   
+
+	}
+
+	@Then("user gets valid output")
+	public void user_gets_valid_output() {
+	   
+
+	}
+
+	@When("user enters invalid code in tryeditor box")
+	public void user_enters_invalid_code_in_tryeditor_box() {
+	   
+
+	}
+
+	@Then("an error dialog box appears")
+	public void an_error_dialog_box_appears() {
+	   
+
+	}
+
+	@Then("user clicks ok button inside dialog box")
+	public void user_clicks_ok_button_inside_dialog_box() {
+	   
+
+	}
+
+	@Then("user in the same tryEditor page with same code writte inside the tryEditor box")
+	public void user_in_the_same_try_editor_page_with_same_code_writte_inside_the_try_editor_box() {
+	   
+
+	}
+
+	@Given("User is in array page")
+	public void user_is_in_array_page() {
+	   
+
+	}
+
+	@When("clicks Array Using List text")
+	public void clicks_array_using_list_text() {
+	   
+
+	}
+
+	@Then("user is directed to arrays-using-list page")
+	public void user_is_directed_to_arrays_using_list_page() {
+	   
+
+	}
+
+	@Given("user is already in arrays-using-List page")
+	public void user_is_already_in_arrays_using_list_page() {
+	   
+
+	}
+
+	@When("clicks Basic Operations in Lists Page text")
+	public void clicks_basic_operations_in_lists_page_text() {
+	   
+
+	}
+
+	@Then("user is directed into basic-operations-in-lists page")
+	public void user_is_directed_into_basic_operations_in_lists_page() {
+	   
+
+	}
+
+	@Given("user is already in basic-operations-in-lists page")
+	public void user_is_already_in_basic_operations_in_lists_page() {
+	   
+
+	}
+
+	@When("clicks Applications of Array page")
+	public void clicks_applications_of_array_page() {
+	   
+
+	}
+
+	@Then("user is directed to applications-of-array page")
+	public void user_is_directed_to_applications_of_array_page() {
+	   
+
+	}
+
+
+	
+
+}
