b4845d19968f58a5c87c362f0b88f4e73b4b11a6
Ajila K Johnkutty <kjajila@gmail.com>
2023-10-02 16:17:32 -0400

    complete datastructure feature file

---
+++ src/test/java/step_definition/DataStructure.java
@@ -0,0 +1,111 @@
+package step_definition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class DataStructure {
+	@Given("An existing user is in the Home page after logged in to check Getting Started button in DataStructures Pane")
+	public void an_existing_user_is_in_the_home_page_after_logged_in_to_check_getting_started_button_in_data_structures_pane() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks the Getting Started button in DataStructures Pane")
+	public void the_user_clicks_the_getting_started_button_in_data_structures_pane() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user will directed to DataStructures Page")
+	public void the_user_will_directed_to_data_structures_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("user is in Data Structures-Introduction page")
+	public void user_is_in_data_structures_introduction_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("clicks Time Complexity button")
+	public void clicks_time_complexity_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("user enters time-complexity page")
+	public void user_enters_time_complexity_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks Try Here button for Data Structures-Introduction page")
+	public void the_user_clicks_try_here_button_for_data_structures_introduction_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should be redirected to a tryEditor page for Data Structures-Introduction page")
+	public void the_user_should_be_redirected_to_a_try_editor_page_for_data_structures_introduction_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("user write some valid java code for Data Structures-Introduction page then click run button")
+	public void user_write_some_valid_java_code_for_data_structures_introduction_page_then_click_run_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("an alert with NameError: name System is not defined on line {int} will display for Data Structures-Introduction page")
+	public void an_alert_with_name_error_name_system_is_not_defined_on_line_will_display_for_data_structures_introduction_page(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("user write some invalid code for  Data Structures-Introduction then click run button")
+	public void user_write_some_invalid_code_for_data_structures_introduction_then_click_run_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("an alert with SyntaxError: bad input on line {int} will display for Data Structures-Introduction")
+	public void an_alert_with_syntax_error_bad_input_on_line_will_display_for_data_structures_introduction(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("user write some python code for Data Structures-Introduction page as input then click run button")
+	public void user_write_some_python_code_for_data_structures_introduction_page_as_input_then_click_run_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The output will be display in the editor for Data Structures-Introduction page")
+	public void the_output_will_be_display_in_the_editor_for_data_structures_introduction_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("user is in time complexity page")
+	public void user_is_in_time_complexity_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("clicks practice Questions for  Data Structures-Introduction")
+	public void clicks_practice_questions_for_data_structures_introduction() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("user is navigated to Practice page for Data Structures-Introduction")
+	public void user_is_navigated_to_practice_page_for_data_structures_introduction() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+}
--- src/test/resources/features/04dataStructures.feature
+++ src/test/resources/features/04dataStructures.feature
@@ -1,35 +1,39 @@
 Feature: feature to test module1: DataStructures-Introduction
 
-  @tag1
-  Scenario: to validate user gets into data structure-introduction page
-    Given the user is in homepage
-    And clicks datastructure Get started button
-    Then user enters data structure-introduction page
-
+   @tag1 
+  Scenario: An existing user logged in with valid credentials in homepage to check Getting Started button in DataStructures Pane
+	  Given An existing user is in the Home page after logged in to check Getting Started button in DataStructures Pane
+	  When The user clicks the Getting Started button in DataStructures Pane
+	  Then The user will directed to DataStructures Page
+	  
   @tag2
   Scenario: to test Time Complexity button
     Given user is in Data Structures-Introduction page
     When clicks Time Complexity button
     Then user enters time-complexity page
 
-  @tag3
-  Scenario: to test Python code is working
-    Given user is in time complexity page
-    And clicks Try here>>> button
-    When enters valid python code in tryEditor box
-    And clicks Run button
-    Then the code is executed successfully.
-
+	@tag3  
+  Scenario: A logged in user is in the Data Structures-Introduction page to check tryeditor
+    When The user clicks Try Here button for Data Structures-Introduction page
+    Then The user should be redirected to a tryEditor page for Data Structures-Introduction page
+    
   @tag4
-  Scenario: to test with invalid python code
-    Given user is in time complexity page
-    And clicks Try here>>> button
-    When enters invalid python code in tryEditor box
-    And clicks Run button
-    Then an error message is displayed
-
+  Scenario: to test with Valid java code
+    When user write some valid java code for Data Structures-Introduction page then click run button
+    Then an alert with NameError: name System is not defined on line 1 will display for Data Structures-Introduction page
+    
   @tag5
+  Scenario: 5to test with invalid code
+    When user write some invalid code for  Data Structures-Introduction then click run button
+    Then an alert with SyntaxError: bad input on line 1 will display for Data Structures-Introduction
+    
+ @tag6
+  Scenario: to test valid Python code
+     When user write some python code for Data Structures-Introduction page as input then click run button
+    Then The output will be display in the editor for Data Structures-Introduction page
+
+  @tag7
   Scenario: to validate user can navigate to Practice Questions
     Given user is in time complexity page
-    When clicks practice Questions
-    Then user is navigated to Practice page
+    When clicks practice Questions for  Data Structures-Introduction
+    Then user is navigated to Practice page for Data Structures-Introduction
